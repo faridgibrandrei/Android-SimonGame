6869580a767149a83797b3ca0a62a9b26779f003
Farid Gibran <farid.gibran@vostra.co.id>
2023-08-22 10:40:05 +0700

    initial commit

---
+++ app/src/main/java/drei/made/simon/MainActivity.java
@@ -0,0 +1,14 @@
+package drei.made.simon;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
